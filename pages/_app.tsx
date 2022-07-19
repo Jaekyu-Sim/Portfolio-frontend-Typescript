9670714b4b9e868aed0e6d2b35d4a51a86b64e11
jaekyu-sim <worb1605@naver.com>
2022-07-20 01:20:02 +0900

    build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Head>
+      <title>개발자 심재규 입니다.</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
